452719bed3ab1986f72ad2768971f0b50032b03c
Kem Attang <kemeattang@gmail.com>
2019-06-06 17:26:21 -0500

    facility No_SQL repo

---
+++ src/main/java/edu/depaul/cdm/se/demo/Repositories/Facility_NOSQL_Repo.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se.demo.Repositories;
+
+import edu.depaul.cdm.se.demo.model.Facility_NoSQL;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface Facility_NOSQL_Repo extends CrudRepository<Facility_NoSQL, String>{
+
+    List<Facility_NoSQL>  findByFacilityName(String FacilityName);
+    List<Facility_NoSQL> findAll();
+}
